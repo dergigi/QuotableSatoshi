98a1124c9fec17b1957cec77866b7c67f0b5616c
Gigi <dergigi@pm.me>
2019-01-21 19:42:32 -0600

    Add character limit and post_to_twitter flag to config

--- src/config.js
+++ src/config.js
@@ -5,4 +5,6 @@ module.exports = {
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  character_limit: 280,
+  post_to_twitter: process.env.POST_TO_TWITTER,
 }
